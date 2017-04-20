792c6dca0e53f13f0b991e3ac0cb4290bc7e7530
Edgar Morales <edmorales94@gmail.com>
2017-04-20 14:58:31 -0400

    Add files via upload
    Game is now in charge of implementing page flipping technique in its draw method to handle the flickering caused by the repaint method called by the operating system

--- Game.java
+++ Game.java
@@ -1,36 +1,66 @@
 package Dash;
 
 import java.awt.Graphics;
+//import java.awt.Toolkit;
 
 public class Game extends GamePanel{
 	
 	/**
 	 * 
 	 */
+	
+	
 	private static final long serialVersionUID = 1L;
-
-	Player batman = new Player(400, 900,0);
+//---------- character instance -----------------------------------------------------------------------------------------------------------------
+	Player batman = new Player(100, 900,0);
+	
+/***************************************************************************************************************************************
+ * startTheGame calls a method in GamePanel that sets up the screen
+ */
 	public void startTheGame(){
 		startFrame();
 	}
 
+/****************************************************************************************************************************************
+ * respondToInput is used to determine what to do when the user presses keys or mouse
+ */
 	@Override
 	public void respondToInput() {
-		batman.respondToInput(input);
 		batman.moveForward();
+		batman.jump();
 	}
 
+/****************************************************************************************************************************************
+ * handleCollisions is used to determine what to do when the character collides
+ */
 	@Override
 	public void handleCollisions() {
 		
 	}
 	
+/****************************************************************************************************************************************
+ * draw calls the update method inherited from GamePanel
+ */
 	@Override
-	public void draw(Graphics g) {
-		super.paintComponent(g);
+	public void draw() {
+		flipPages = getBufferStrategy();//get a reference of the bufferStrategy created in GamePanel
+		if(flipPages == null){//if bufferStrategy is empty
+			createBufferStrategy(3);//create a new one
+			return;
+		}
+		
+		Graphics g = flipPages.getDrawGraphics();//get the graphics object
+		super.update(g);//The canvas is first cleared by filling it with the background color, and then completely redrawn by calling this canvas's paint method
 		batman.draw(g);
+		
+		g.dispose();
+		flipPages.show();
+		//Toolkit.getDefaultToolkit().sync();
 	}
 	
+/****************************************************************************************************************************************
+	 * respondToInput is used to determine what to do when the user presses keys or mouse
+	 */
 	public static void main(String[] args) {
 		Game game = new Game();
 		game.startTheGame();
