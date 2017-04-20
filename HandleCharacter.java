d2d53cc3a8c3363f1a2ddb72cde29049af49e9af
Edgar Morales <edmorales94@gmail.com>
2017-04-19 21:24:15 -0400

    Add files via upload
    A totally function jump method was added to this class. This method takes advantage of the Thread.run method to increase and decrease the value of y to make the character look like it is jumping.

--- HandleCharacter.java
+++ HandleCharacter.java
@@ -6,13 +6,25 @@ public abstract class HandleCharacter {
 	
 	double x;// x-coordinate where the character will be placed
 	double y;// y-coordinate where the character will be placed
+		
 	
-	int angle;//angle where the character will be facing 
+	static int angle;//angle where the character will be facing 
+	
+	static double fallingSpeed = 0;
+	double gravity = 2;
+	
+	double currentTime = System.currentTimeMillis();
+	static boolean onGround = false;
+	static boolean canRotate = false;
 	
 	int[][] allXCoordinates = getAllXCoordinates();// contains all Xs of each polygon
 	int[][] allYCoordinates = getAllYCoordinates();// contains all Ys of each polygon
 	
-//---------- constructor to initialize global variable -------------------------------------------------------------------------
+/*******************************************************************************************************************************************************
+ * Constructor will initialize a batman character
+ * with the (int x,int y, int degrees) positions and angle where it'll be facing
+ */
+	@SuppressWarnings("static-access")
 	public HandleCharacter(double x, double y, int angle){
 		this.x = x;
 		this.y = y;
@@ -23,21 +35,63 @@ public abstract class HandleCharacter {
 	public abstract int[][] getAllXCoordinates();
 	public abstract int[][] getAllYCoordinates();
 
-//---------- rotate method -----------------------------------------------------------------------------------------------------
-	public void rotate(){
-		angle += 90;//rotates the character clockwise 90 degrees
-		if(angle > 359){angle -= 360;}
+/*******************************************************************************************************************************************************
+ * this function will rotate the coordinates of the character
+ * 90 degrees clockwise
+ */
+	public static void rotate(){
+		if(canRotate){
+		angle += 90;//increase the angle 90 degrees
+		if(angle > 359){
+			angle -= 360;}//we decrease the degress to be inside a range from 0-359 degress
+		}
 	}
 
-//---------- moveForward method ------------------------------------------------------------------------------------------------
+/*******************************************************************************************************************************************************
+ * moveForward will move the character forward 
+ * every time this function is called by a fixed amount of ints
+ */
 	public void moveForward(){
-		x += 5;
+		x += 15;
 	}
-//---------- draw method -------------------------------------------------------------------------------------------------------
+
+/*******************************************************************************************************************************************************
+ * startToJump will initialize the speed for the jump/fall
+ */
+	public static void startToJump(){
+		if(onGround){//if character is on ground,the falling speed changes to negative value
+			fallingSpeed = -30.0;//it is negative so y-coordinate goes down("up")
+			canRotate = false;//while jumping, you can't rotate anymore
+			onGround = false;//you are not longer on the ground
+		}
+	}
+	
+/*******************************************************************************************************************************************************
+ * jump will add negative and positive numbers to the y coordinate to make it look
+ * like the character is jumping
+ */
+	public void jump(){
+		fallingSpeed += gravity;//we add gravity(positive) to fallingSpeed(negative)
+		y += fallingSpeed;//change y value according to fallingSpeed
+		//falling speed starts negative but it later becomes positive, so y goes up and down
+		if(y > 900){//if the y position is higher than 900
+			y = 900;//set it back to initial position
+			fallingSpeed = 0;//no more "force" on the y coordinate
+			onGround = true;//back on the ground again
+			canRotate = true;//once on the ground, it can rotate at the moment of jumping
+		}
+	}
+	
+/*******************************************************************************************************************************************************
+ * Draw method will handle the position of the x and y coordinates 
+ * of each single polygon that composes the face of the character
+ * during rotation and moving forward functions.
+ * It also handles the color for each shape of the face
+ */
 	public void draw(Graphics g){
 
-		int[] xPoints;
-		int[] yPoints;
+		int[] xPoints;//array to place the x-coordinates of each polygon composing the head individually
+		int[] yPoints;//array to place the y-coordinates of each polygon composing the head individually
 		
 		double cosAngle = Lookup.cos[angle];//will help to set the correct x-value for the position
 		double sinAngle = Lookup.sin[angle];//will help to set the correct y-value for the polygon
@@ -60,8 +114,7 @@ public abstract class HandleCharacter {
 			if(polygon == 4){ color = Color.black;}//mouth
 			
 			g.setColor(color);
-			g.drawPolygon(xPoints, yPoints,allXCoordinates[polygon].length);
-			//g.fillPolygon(xPoints, yPoints, allXCoordinates[polygon].length);			
+			g.fillPolygon(xPoints, yPoints, allXCoordinates[polygon].length);			
 		}
 	}
 
