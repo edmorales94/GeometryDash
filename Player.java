3fe4b3cbf0052a8944f2a3c950ba4d8624c36b96
Edgar Morales <edmorales94@gmail.com>
2017-04-26 17:14:17 -0400

    Y-coordinates were updated

--- Player.java
+++ Player.java
@@ -28,7 +28,7 @@ public class Player extends HandleCharacter{
 	public int[][] getAllYCoordinates() {
 		int[][] allYCoordinates = 
 			{ 
-			 {-90,-40,-40,-90,90,90},//y-coordinates for the head
+			 {-100,-40,-40,-100,100,100},//y-coordinates for the head
 			 {-12,0,12,0},//y-coordinates for left eye
 			 {-12,0,12,0},//y-coordinates for right eye
 			 {30,55,30,60,55,85,85,55,60},//x-coordinates for the skin section
@@ -36,14 +36,9 @@ public class Player extends HandleCharacter{
 		};
 		return allYCoordinates;
 	}
-	
 //---------- draw method ------------------------------------------------------------------------------------------------------------------------
 	public void draw(Graphics g){
 		super.draw(g);
 	}
 }
 
-
-
-//Arianna.FloresPerez14@bcacad.local
-//112314114
