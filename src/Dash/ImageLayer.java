22909492b92bebbfc1debf6427120c9ece72d339
Edgar Morales <edmorales94@gmail.com>
2017-05-12 00:23:52 -0400

    Constructor now requires number of pictures desired
    It no longer requires an angle

--- src/Dash/ImageLayer.java
+++ src/Dash/ImageLayer.java
@@ -18,9 +18,10 @@ public class ImageLayer {
 	double z;//z is used to make it look like is far away from the camera
 	
 	int width;//picture's width
+	int numberOfImages;//this asks how many pictures the user wants to place
 	
 //---------- constructor ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-	public ImageLayer(String filename, double x, double y, double z, int width){
+	public ImageLayer(String filename, double x, double y, double z, int width, int numPictures){
 		//TookKit loads the image in another thread, so it takes longer to paint in the Game.java 
 		
 		image = new ImageIcon(filename).getImage();//ImageIcon loads the image and waits for it to 
@@ -29,6 +30,7 @@ public class ImageLayer {
 		this.y = y;//initializing y coordinate
 		this.z = z;//initializing the distance from this picture and camera
 		this.width = width;//setting the width
+		numberOfImages = numPictures;//setting the number of pictures
 	}
 	
 /******************************************************************************************************************************************************************************************************
@@ -36,7 +38,7 @@ public class ImageLayer {
  * @param g
  */
 	public void draw(Graphics g){
-		for(int i = 0; i < 4; i++){//places 4 continous pictures
+		for(int i = 0; i < numberOfImages; i++){//places 4 continous pictures
 			g.drawImage(image, (int)(x - Camera.x/z)+width*i, (int)(y), null);
 			//placing the image, adjusting x according to depth, y location, and ImageObserver
 		}
