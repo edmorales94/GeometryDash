99372d0166705e9bcb567e858777e25055ba533f
Edgar Morales <edmorales94@gmail.com>
2017-05-12 00:13:50 -0400

    This class handles audio files

---
+++ src/Dash/AudioPlayer.java
@@ -0,0 +1,65 @@
+package Dash;
+
+import javax.sound.sampled.*;
+
+public class AudioPlayer {
+	
+	private Clip clip;
+	
+/*****************************************************************************************************************************************************************
+ * Constructs an audio input stream that reads its data from the target indicated
+ * @param filename
+ */
+	public AudioPlayer(String filename){
+		
+		try {
+			AudioInputStream audio = AudioSystem.getAudioInputStream(getClass().getResourceAsStream(filename));
+			AudioFormat baseFormat = audio.getFormat();
+			AudioFormat decodeFormat =  new AudioFormat(
+					AudioFormat.Encoding.PCM_SIGNED, 
+					baseFormat.getSampleRate(), 
+					16, 
+					baseFormat.getChannels(), 
+					baseFormat.getChannels()*2, 
+					baseFormat.getSampleRate(), 
+					false);
+			
+			AudioInputStream decodeAudio = AudioSystem.getAudioInputStream(decodeFormat,audio);
+			clip = AudioSystem.getClip();
+			clip.open(decodeAudio);
+		} 
+		
+		catch (Exception e) {
+			System.out.println("Couldn't load audio");
+		} 
+	}
+	
+/*****************************************************************************************************************************************************************
+ * This method stops the file that is playing
+ */
+	public void stop(){
+		if(clip.isRunning()){//if the audio is still playing
+			clip.stop();//then stop it
+		}
+	}
+	
+/*****************************************************************************************************************************************************************
+ * This method will play a file as long as it isn't empty
+ */
+	public void play(){
+		if(clip == null){//if the file is empty
+			return;//get out of the method
+		}
+		stop();//stop the file that is playing
+		clip.setFramePosition(0);//restart the file
+		clip.start();//start audio again
+	}
+	
+/*****************************************************************************************************************************************************************
+ * This stops the audio for good
+ */
+	public void closeMusic(){
+		stop();//stop the audio
+		clip.close();//and close it
+	}
+}
